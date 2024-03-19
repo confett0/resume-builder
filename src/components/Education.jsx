04e750450ee7a44fd10276bcc8d7e6421b3bcc25
confett0 <118047961+confett0@users.noreply.github.com>
2024-03-19 18:35:52 +0100

    Create EducationForm component

---
+++ src/components/Education.jsx
@@ -0,0 +1,52 @@
+export default function EducationForm(){
+    return (
+        <>
+        <label>
+        School: 
+        <input
+          value={}
+          name="school"
+          onChange={}
+          placeholder="Enter school / university"
+        />
+      </label>
+      <label>
+        Title: 
+        <input
+          value={}
+          name="title"
+          onChange={}
+          placeholder="Enter degree / field of study"
+        
+        />
+      </label>
+      <label>
+        Start Date: 
+        <input
+          value={}
+          name="startDate"
+          onChange={}
+          placeholder="Enter Start Date"
+        />
+      </label>
+      <label>
+        End Date: 
+        <input
+          value={}
+          name="endDate"
+          onChange={}
+          placeholder="Enter End Date"
+        />
+      </label>
+      <label>
+        Location: 
+        <input
+          value={}
+          name="location"
+          onChange={}
+          placeholder="Enter Location"
+        />
+      </label>
+        </>
+    )
+}
