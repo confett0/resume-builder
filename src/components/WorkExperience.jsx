93f4f3e6ca2ac4b95575c7ac0f217b5b52800c7c
confett0 <118047961+confett0@users.noreply.github.com>
2024-03-23 19:41:26 +0100

    Create work experience form

---
+++ src/components/WorkExperience.jsx
@@ -0,0 +1,61 @@
+export default function WorkForm(){
+    return (
+        <>
+        <label>
+        Company Name: 
+        <input
+          value={}
+          name="company"
+          onChange={}
+          placeholder="Enter Company Name"
+        />
+      </label>
+      <label>
+        Position Title: 
+        <input
+          value={}
+          name="positionTitle"
+          onChange={}
+          placeholder="Enter Position Title"
+        
+        />
+      </label>
+      <label>
+        Start Date: 
+        <input
+          value={}
+          name="startDate"
+          onChange={}
+          placeholder="Enter Start Date"
+        />
+      </label>
+      <label>
+        End Date: 
+        <input
+          value={}
+          name="endDate"
+          onChange={}
+          placeholder="Enter End Date"
+        />
+      </label>
+      <label>
+        Location: 
+        <input
+          value={}
+          name="location"
+          onChange={}
+          placeholder="Enter Location"
+        />
+      </label>
+      <label>
+        Description: 
+        <textarea
+          value={}
+          name="description"
+          onChange={}
+          placeholder="Enter job description"
+        />
+      </label>
+        </>
+    )
+}
