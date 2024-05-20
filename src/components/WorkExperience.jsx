581ba42878ca3507989e549efd966157f5a0f308
confett0 <118047961+confett0@users.noreply.github.com>
2024-05-20 10:50:52 +0200

    Edit work form

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -1,63 +1,68 @@
-export default function WorkForm(){
-    return (
-        <>
-        <form>
+export default function WorkForm({
+  formData,
+  handleChange,
+  handleSubmit,
+  isEditing,
+}) {
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
         <label>
-        Company Name: 
-        <input
-          // value={}
-          name="company"
-          // onChange={}
-          placeholder="Enter Company Name"
-        />
-      </label>
-      <label>
-        Position Title: 
-        <input
-          //value={}
-          name="positionTitle"
-          //onChange={}
-          placeholder="Enter Position Title"
-        
-        />
-      </label>
-      <label>
-        Start Date: 
-        <input
-          //value={}
-          name="startDate"
-          //onChange={}
-          placeholder="Enter Start Date"
-        />
-      </label>
-      <label>
-        End Date: 
-        <input
-          //value={}
-          name="endDate"
-          //onChange={}
-          placeholder="Enter End Date"
-        />
-      </label>
-      <label>
-        Location: 
-        <input
-          //value={}
-          name="location"
-          //onChange={}
-          placeholder="Enter Location"
-        />
-      </label>
-      <label>
-        Description: 
-        <textarea
-          //value={}
-          name="description"
-          //onChange={}
-          placeholder="Enter job description"
-        />
-      </label>
+          Company Name:
+          <input
+            value={formData.company}
+            name="company"
+            onChange={handleChange}
+            placeholder="Enter Company Name"
+          />
+        </label>
+        <label>
+          Position Title:
+          <input
+            value={formData.position}
+            name="positionTitle"
+            onChange={handleChange}
+            placeholder="Enter Position Title"
+          />
+        </label>
+        <label>
+          Start Date:
+          <input
+            value={formData.startDate}
+            name="startDate"
+            onChange={handleChange}
+            placeholder="Enter Start Date"
+          />
+        </label>
+        <label>
+          End Date:
+          <input
+            value={formData.endDate}
+            name="endDate"
+            onChange={handleChange}
+            placeholder="Enter End Date"
+          />
+        </label>
+        <label>
+          Location:
+          <input
+            value={formData.location}
+            name="location"
+            onChange={handleChange}
+            placeholder="Enter Location"
+          />
+        </label>
+        <label>
+          Description:
+          <textarea
+            value={formData.description}
+            name="description"
+            onChange={handleChange}
+            placeholder="Enter job description"
+          />
+        </label>
+        <button type="submit">{isEditing ? "Save" : "Submit"}</button>
       </form>
-        </>
-    )
+    </>
+  );
 }
